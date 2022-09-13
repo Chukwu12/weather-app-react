30ba5baaccacd7d148ff6e3cb61da7e7b556a87a
okey <39450490+Chukwu12@users.noreply.github.com>
2022-09-13 18:37:39 -0400

    deploy website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
+
+
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https: //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={b710b20aa3d46f59289723c99fe79c9c`
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -17,6 +18,11 @@ function App() {
       setLocation('')
     }
   }
+
+  
+
+
+
 
   return (
     <div className="app">
