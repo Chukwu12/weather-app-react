eab8adb9d2b991d760ad4294b34f2e9114cdf182
okey E <okeysmooed@gmail.com>
2024-02-11 20:51:52 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,81 +1,79 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-
-
+import React, { useState } from 'react';
+import axios from 'axios';
+import sunnyImage from '../src/resources/sunny-weather.jpg'; // Import background images
+import rainyImage from '../src/resources/rainy-weather.jpg';
+import snowyImage from '../src/resources/snowy-weather.jpg';
+import cloudyImage from '../src/resources/cloudy-weather.jpg';
 
 function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const [backgroundImage, setBackgroundImage] = useState(null); // State for background image
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=b710b20aa3d46f59289723c99fe79c9c`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=b710b20aa3d46f59289723c99fe79c9c`;
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      setLocation('')
+        setData(response.data);
+        setBackgroundImage(getBackgroundImage(response.data.weather[0].main)); // Set background image
+      });
+      setLocation('');
     }
-  }
+  };
 
-  // function setBackground(weather) {
-  //   if (weather == "Rain") {
-  //     background.src = "./resources/rainy-weather.jpg";
-  //   } else if (weather == "Snow") {
-  //     background.src = "./resources/snowy-weather.jpg";
-  //   } else if (weather == "Clear") {
-  //     background.src = "./resources/sunny-weather.jpg";
-  //   } else if (weather == "Clouds") {
-  //     background.src = "./resources/cloudy-weather.jpg";
-  //   }
-  // }
-
-
-
+  // Function to get background image based on weather condition
+  const getBackgroundImage = (weather) => {
+    switch (weather) {
+      case 'Rain':
+        return rainyImage;
+      case 'Snow':
+        return snowyImage;
+      case 'Clear':
+        return sunnyImage;
+      case 'Clouds':
+        return cloudyImage;
+      default:
+        return null; // Default background image
+    }
+  };
 
   return (
-    <div className="app">
+    <div className="app" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="search">
         <input
           value={location}
-          onChange={event => setLocation(event.target.value)}
+          onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
+          placeholder="Enter Location"
+          type="text"
+        />
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
           </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+          <div className="temp">{data.main && <h1>{data.main.temp.toFixed()}°F</h1>}</div>
+          <div className="description">{data.weather && <p>{data.weather[0].main}</p>}</div>
         </div>
 
-        {data.name !== undefined &&
+        {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              {data.main && <p className="bold">{data.main.feels_like.toFixed()}°F</p>}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              {data.main && <p className="bold">{data.main.humidity}%</p>}
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.wind && <p className="bold">{data.wind.speed.toFixed()} MPH</p>}
               <p>Wind Speed</p>
             </div>
           </div>
-        }
-
-
-
+        )}
       </div>
     </div>
   );
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,13 @@
-import React from "react";
-import VideoBg from '../assets/video.mp4';
+// import React from "react";
+// import VideoBg from '../assets/video.mp4';
 
-const Main = () => {
-    return (
-        <div className="main">
-            <video src={VideoBg} autoPlay loop muted />
+// const Main = () => {
+//     return (
+//         <div className="main">
+//             <video src={VideoBg} autoPlay loop muted />
         
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Main
+// export default Main
