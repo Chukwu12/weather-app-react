5aa77643e20b2780c548e58167bffe30529846d9
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-02-22 15:20:16 +0000

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom'; // Import createRoot
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import './index.css';
 import App from './App';
 
